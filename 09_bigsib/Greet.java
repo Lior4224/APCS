242dcb745df703400c85b49292a2dcf88ec2e521
Lior Polischouk <67924209+Lior4224@users.noreply.github.com>
2021-10-02 21:29:08 -0400

    Update Greet.java

--- 09_bigsib/Greet.java
+++ 09_bigsib/Greet.java
@@ -1,5 +1,5 @@
 /*
-Lior Polischouk
+Lior Polischouk (Toothless), Alif (Grippy), Josiah ()
 APCS
 HW#09: What Are BigSibs Good For?
 10/2/21
@@ -13,7 +13,10 @@ public class Greet {
 }
 /*
 DISCOVERIES:
+1. Discovered that we can call methods from other files in this file when the files are in the same 
+directory by simply doing <filename>.<methodname> (BigSib.greet)
 
 UNRESOLVED QUESTIONS:
+1. Can you call methods from files not in this directory (but still in the same repository) in this file?
 
 */
