18f76c3552d6be7298355b2ebdf4c91b486d61b3
Lior Polischouk <67924209+Lior4224@users.noreply.github.com>
2021-10-02 21:05:51 -0400

    Create Greet.java

---
+++ 09_bigsib/Greet.java
@@ -0,0 +1,22 @@
+/*
+Lior Polischouk
+APCS
+HW#09: What Are BigSibs Good For?
+10/2/21
+*/
+public class Greet {
+  public static void main (String [] args) {
+    greet("Bob");
+    greet("Jeff");
+    greet("Daniel");
+  }
+  public static void greet (String name) {
+    System.out.println("Greetings, " + name + ", how do you do?");
+  }
+}
+/*
+DISCOVERIES:
+
+UNRESOLVED QUESTIONS:
+
+*/
