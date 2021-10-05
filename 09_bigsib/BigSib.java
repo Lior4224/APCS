cbccf5e59190caf62fb4c8b97e2552c173d86f9b
Lior Polischouk <67924209+Lior4224@users.noreply.github.com>
2021-10-04 20:10:02 -0400

    Update BigSib.java

--- 09_bigsib/BigSib.java
+++ 09_bigsib/BigSib.java
@@ -6,9 +6,7 @@ HW#09: What Are BigSibs Good For?
 */
 public class BigSib {
   public static void main (String [] args) {
-    greet("Bob");
-    greet("Jeff");
-    greet("Daniel");
+
   }
   public static void greet (String name) {
     System.out.println("Greetings, " + name + ", how do you do?");
