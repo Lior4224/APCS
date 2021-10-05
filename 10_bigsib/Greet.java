77ed579f442f5823996a4fc900b2ba1aaf1c6bd6
Lior Polischouk <67924209+Lior4224@users.noreply.github.com>
2021-10-04 20:13:46 -0400

    Create Greet.java

---
+++ 10_bigsib/Greet.java
@@ -0,0 +1,19 @@
+/*
+Lior Polischouk (Toothless), Alif (Grippy), Josiah (Hedwig)
+APCS
+HW#10: Refactor Big Sib One
+10/4/21
+*/
+public class Greet {
+  public static void main (String [] args) {
+    system.out.println(BigSib.greet("Bob"));
+    system.out.println(BigSib.greet("Jeff"));
+    system.out.println(BigSib.greet("Daniel"));
+  }
+}
+/*
+DISCOVERIES:
+1. 
+UNRESOLVED QUESTIONS:
+1. 
+*/
