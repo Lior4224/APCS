3189e8f1f48b82f3c28ad7ba64382c02eae5d7ae
Lior Polischouk <67924209+Lior4224@users.noreply.github.com>
2021-10-05 21:40:31 -0400

    Create BigSib.java

---
+++ 12_bigsib/BigSib.java
@@ -0,0 +1,15 @@
+/*
+Lior Polischouk (Toothless), Alif (Grippy), Josiah (Hedwig)
+APCS
+HW#12: On Elder Individuality and the Elimination of Radio Fuzz
+10/5/21
+*/
+public class BigSib {
+  public static void main (String [] args) {
+
+  }
+  public static String greet (String name) {
+    String statement = ("Greetings, " + name + ", how do you do?");
+    return statement;
+  }
+}
