2faccb06a51e6fbe9e0fa450764b20a4b2980584
Lior Polischouk <67924209+Lior4224@users.noreply.github.com>
2021-10-05 21:49:02 -0400

    Update BigSib.java

--- 12_bigsib/BigSib.java
+++ 12_bigsib/BigSib.java
@@ -9,7 +9,9 @@ public class BigSib {
 
   }
   public static String greet (String name) {
+    String helloMsg;
     String statement = ("Greetings, " + name + ", how do you do?");
+    String helloMsg = statement;
     return statement;
   }
 }
