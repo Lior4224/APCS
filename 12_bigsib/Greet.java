fbe1ccc90566e808413cc62643f3fbc072a3a526
Lior Polischouk <67924209+Lior4224@users.noreply.github.com>
2021-10-05 21:48:58 -0400

    Create Greet.java

---
+++ 12_bigsib/Greet.java
@@ -0,0 +1,27 @@
+/*
+Lior Polischouk (Toothless), Alif (Grippy), Josiah (Hedwig)
+APCS
+HW#12: On Elder Individuality and the Elimination of Radio Fuzz
+10/5/21
+*/
+public class Greet {
+  public static void main (String [] args) {
+    String greeting;
+    
+    BigSib richard = new BigSib();
+    richard.setHelloMsg("Word up");
+    greeting = richard.greet("freshman");
+    System.out.println(greeting);
+    
+/*    System.out.println( BigSib.greet("Bob") );
+    System.out.println( BigSib.greet("Jeff") );
+    System.out.println( BigSib.greet("Daniel") );
+    */
+  }
+}
+/
+DISCOVERIES:
+1.
+UNRESOLVED QUESTIONS:
+1. none
+*/
