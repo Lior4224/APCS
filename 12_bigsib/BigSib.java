f61773f7eb15f9be30b0401d41b56aeb0397958d
Lior Polischouk <67924209+Lior4224@users.noreply.github.com>
2021-10-06 12:02:17 -0400

    Update BigSib.java

--- 12_bigsib/BigSib.java
+++ 12_bigsib/BigSib.java
@@ -4,6 +4,8 @@ APCS
 HW#12: On Elder Individuality and the Elimination of Radio Fuzz
 10/5/21
 */
+
+/*Add a constructor to class BigSib that would allow Greet to run without richard.SetHelloMsg("Word up") */
 public class BigSib {
   String helloMsg;
   public static void main (String [] args) {
