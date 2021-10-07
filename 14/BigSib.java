d8ca0af306a668dea8b030f2a835ed4152a785c6
Lior Polischouk <67924209+Lior4224@users.noreply.github.com>
2021-10-07 12:06:40 -0400

    Create BigSib.java

---
+++ 14/BigSib.java
@@ -0,0 +1,15 @@
+public class BigSib {
+  String helloMsg;
+  public BigSib(String phrase) {
+    helloMsg = phrase;
+  }
+
+  public String greet (String name) {
+    return (helloMsg + " " + name);
+
+  }
+  public String setHelloMsg (String str) {
+    helloMsg = str;
+    return helloMsg;
+  }
+}
