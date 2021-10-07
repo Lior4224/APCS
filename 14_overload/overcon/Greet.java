11c80c94c2b6bf6246e400cd97191f776b25458f
Lior Polischouk <67924209+Lior4224@users.noreply.github.com>
2021-10-07 16:53:19 -0400

    Create Greet.java

---
+++ 14_overload/overcon/Greet.java
@@ -0,0 +1,24 @@
+/*
+Lior Polischouk , Jomin, Brian
+APCS
+HW#14: Customize Your Creation
+10/7/21
+*/
+public class Greet {
+  public static void main( String[] args ) {
+    String greeting;
+    BigSib richard = new BigSib("Word up");
+    greeting = richard.greet("freshman");
+    System.out.println(greeting);
+    
+    BigSib tracy = new BigSib("Salutations);
+    greeting = tracy.greet("Dr.Spaceman");
+    System.out.println(greeting);
+  }
+}
+/*
+DISCOVERIES:
+1.
+QUESTIONS:
+1.
+*/
