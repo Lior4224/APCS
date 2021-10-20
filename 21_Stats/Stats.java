b6ec1f15e4d205454413d29da059d9218a3c7841
Lior Polischouk <67924209+Lior4224@users.noreply.github.com>
2021-10-20 19:21:54 -0400

    Update Stats.java

--- 21_Stats/Stats.java
+++ 21_Stats/Stats.java
@@ -21,39 +21,53 @@ public class Stats {
     return mean;
   }
 
-//   public static int max(int a, int b) {
-//     if a > b {
-//       return a
-//       }
-//     else {
-//       return b
-//       }
-//   }
+  public static int max(int a, int b) {
+    if a >= b {
+      return a
+      }
+    else {
+      return b
+      }
+  }
 
-//   public static double max(double a, double b) {
-//     if a > b {
-//       return a
-//       }
-//     else {
-//       return b
-//       }
-//   }
+  public static double max(double a, double b) {
+    if a >= b {
+      return a
+      }
+    else {
+      return b
+      }
+  }
 
-//   public static int geoMean(int a, int b) {
+  public static int geoMean(int a, int b) {
+     int mean = Math.sqrt (a * b);
+     return mean;
+  }
 
-//   }
+  public static double geoMean(double a, double b) {
+     double mean = Math.sqrt (a * b);
+     return mean;
+  }
 
-//   public static double geoMean(double a, double b) {
+  public static int max(int a, int b, int c) {
+     if (a >= b) && (a >= c){
+        return a;
+     }else if (b >= a) && (b >= c){
+        return b;
+     }else {
+        return c;
+     }
+  }
 
-//   }
-
-//   public static int max(int a, int b, int c) {
-
-//   }
-
-//   public static double max(double a, double b, double c) {
-
-//   }
+  public static double max(double a, double b, double c) {
+     if (a >= b) && (a >= c){
+        return a;
+     }else if (b >= a) && (b >= c){
+        return b;
+     }else {
+        return c;
+     }
+  }
 
 //   public static int geoMean(int a, int b, int c) {
 
@@ -66,9 +80,11 @@ public class Stats {
 
 //   //main method for testing functionality
   public static void main( String[] args ) {
-    System.out.println(mean((int) 1,(int) 5));
-    System.out.println(mean((double) 1,(double) 5));
-    System.out.println(mean((double) 1,(double) 4));
+     System.out.println(mean((int) 1,(int) 5));
+     System.out.println(mean((double) 1,(double) 5));
+     System.out.println(mean((double) 1,(double) 4));
+     System.out.println(max((double) 2,(double) 3));
+     System.out.println(max((int) 2,(int) 3));
 
   }
 
