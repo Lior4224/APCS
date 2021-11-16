cb2aea1eed1ca3bd6b19b53f934313e85b2d22c4
Lior Polischouk <67924209+Lior4224@users.noreply.github.com>
2021-11-16 18:45:15 -0500

    Update Array1.java

--- cb/array-1/Array1.java
+++ cb/array-1/Array1.java
@@ -9,21 +9,28 @@ DISCO
 QCC
 -
 */
-public class Array1 {
+public class Array1 { 
     public static boolean firstLast6(int[] nums) {
+      //Given an array of ints, return true if 6 appears as either the first or last element in the array. 
+      //The array will be length 1 or more.
       return (nums[0]==6 || nums[nums.length - 1]==6);
     }
     public static boolean sameFirstLast(int[] nums) {
+      //Given an array of ints, return true if the array is length 1 or more, and the first element and the last element are equal.
       return (nums.length > 0 && nums[0]==nums[nums.length - 1]);
     }
     public static int[] makePi() {
+      //Return an int array length 3 containing the first 3 digits of pi, {3, 1, 4}.
       int [] pi = {3, 1, 4};
       return pi;
     }
     public static boolean commonEnd(int[] a, int[] b) {
+      //Given 2 arrays of ints, a and b, return true if they have the same first element or they have the same last element. 
+      //Both arrays will be length 1 or more.
       return (a[0]==b[0] || a[a.length - 1]==b[b.length - 1]);
     }
     public static int sum3(int[] nums) {
+      //Given an array of ints length 3, return the sum of all the elements.
       int local = 0;
       for (int element : nums) {
         local += element;
