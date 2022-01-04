29ba16fe96da611ac2cdc3e5c220505ecace161f
Lior Polischouk <67924209+Lior4224@users.noreply.github.com>
2022-01-03 19:20:26 -0500

    Update BubbleSort.java

--- 51_bubblesort/BubbleSort.java
+++ 51_bubblesort/BubbleSort.java
@@ -2,7 +2,7 @@
 Big Apples (Weichen Liu + Blueface, Jeffery Tang + Mathias, Lior + Toothless)
 APCS pd6
 HW49 -- Java Conventions
-2021-12-22
+2022-01-03
 time spent:  0.5 hours
 
  * class BubbleSort -- implements bubblesort algorithm (vanilla)
