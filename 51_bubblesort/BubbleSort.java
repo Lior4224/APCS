40b542ce4fafc6001c7d34a243c832553536675c
Lior Polischouk <67924209+Lior4224@users.noreply.github.com>
2022-01-04 19:40:39 -0500

    Update BubbleSort.java

--- 51_bubblesort/BubbleSort.java
+++ 51_bubblesort/BubbleSort.java
@@ -1,15 +1,19 @@
 /*
 Big Apples (Weichen Liu + Blueface, Jeffery Tang + Mathias, Lior + Toothless)
 APCS pd6
-HW49 -- Java Conventions
-2022-01-03
+HW51 -- Working on BubbleSorting
+2021-01-03
 time spent:  0.5 hours
 
  * class BubbleSort -- implements bubblesort algorithm (vanilla)
  *
  * ALGO:
+ * In each pass of the arraylist, we use an if statement inside a for loop to compare consecutive elements on the arraylist. 
+ * If index x is less than the index x - 1, they swap places.
+ * We keep passing through the arraylist until we reach the amount of passes that occured. 
  *
  * DISCO
+ * The input is a class ArrayList, so we have to use the get() to access the elements of the arraylist. 
  *
  * QCC
  * q0: If a pass requires no swaps, what do you know?
@@ -60,12 +64,14 @@ public class BubbleSort
   // postcondition: data's elements sorted in ascending order
   public static void bubbleSortV( ArrayList<Comparable> data )
   {
-    for ( int i = 0 ; i < data.length - 1 ; i++) {
-      for ( int x = data.length - 1 ; x > 0 ; x--) {
-        if (data[x].equals(data[x-1] < 0) {
-          Comparable n = data[x - 1];
-          data[x-1] = data[x];
-          data[x] = n;
+    for ( int i = 0 ; i < data.size() - 1 ; i++) {
+      // Amount of times we pass through the entire arraylist
+      for ( int x = data.size() - 1 ; x > i ; x--) {
+        // Allows us to compare consecutive values of elements on the array list
+        if ((data.get(x).compareTo(data.get(x-1)) < 0)) { 
+          Comparable n = data.get(x-1);
+          data.set(x, data.get(x-1));
+          data.set(x-1, n);
         }
       }
     }
@@ -77,14 +83,19 @@ public class BubbleSort
   //                Returns sorted copy of input ArrayList.
   public static ArrayList<Comparable> bubbleSort( ArrayList<Comparable> input )
   {
-    /* YOUR IMPLEMENTATION HERE */
+    ArrayList copy = new ArrayList<Comparable>(); 
+    
+    for (Comparable i : input) {
+      copy.add(i); 
+    }
+    bubbleSortV(copy);
+    return copy; 
   }
 
 
   public static void main( String [] args )
   {
-
-    /*===============for VOID methods=============
+      /*===============for VOID methods=============
       ArrayList glen = new ArrayList<Integer>();
       glen.add(7);
       glen.add(1);
@@ -98,9 +109,10 @@ public class BubbleSort
       System.out.println( "ArrayList coco before sorting:\n" + coco );
       bubbleSortV(coco);
       System.out.println( "ArrayList coco after sorting:\n" + coco );
+
       ============================================*/
 
-    /*==========for AL-returning methods==========
+
       ArrayList glen = new ArrayList<Integer>();
       glen.add(7);
       glen.add(1);
@@ -111,14 +123,14 @@ public class BubbleSort
       ArrayList glenSorted = bubbleSort( glen );
       System.out.println( "sorted version of ArrayList glen:\n"
       + glenSorted );
-      System.out.println( "ArrayList glen after sorting:\n" + glen );
+      System.out.println( "ArrayList glen after sorting:\n" + glenSorted );
       ArrayList coco = populate( 10, 1, 1000 );
       System.out.println( "ArrayList coco before sorting:\n" + coco );
       ArrayList cocoSorted = bubbleSort( coco );
       System.out.println( "sorted version of ArrayList coco:\n"
       + cocoSorted );
       System.out.println( "ArrayList coco after sorting:\n" + coco );
-      System.out.println( coco );
+        /*==========for AL-returning methods==========
       ============================================*/
 
   }//end main
