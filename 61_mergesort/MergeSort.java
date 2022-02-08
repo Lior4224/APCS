b98137451a2896c32364bc1cc2922cfcc2db3e04
Lior Polischouk <67924209+Lior4224@users.noreply.github.com>
2022-02-08 18:51:16 -0500

    Update MergeSort.java

--- 61_mergesort/MergeSort.java
+++ 61_mergesort/MergeSort.java
@@ -1,4 +1,10 @@
 /***
+  Lior Polischouk and Lindsay Phung
+  APCS
+  HW 61 -- Instructions so Simple... / merge and sort array of Implements
+  2022-02-08
+  time spent: 1 hr
+
   class MergeSort
   Implements mergesort on array of ints.
 
@@ -17,12 +23,31 @@ public class MergeSort
    ******************************************************/
   private static int[] merge( int[] a, int[] b )
   {
-    int[] full = new int[a.length+b.length];
-    for (int i = 0; i < full.length; i++) {
-      if (a.length == 0) {
-        full[i] = b[i];
+    int[] full = new int[a.length + b.length];
+    int i = 0;
+    int j = 0;
+    int k = 0;
+    for (;i < full.length && j != a.length && k != b.length; i++) {
+      if (a[j] > b[k]) {
+        full[i] = b[k];
+        k ++;
       }
-      else if 
+      else {
+        full[i] = a[j];
+        j ++;
+      }
+    }
+    for (int l = i; l < full.length; l++) {
+      if (j == a.length) {
+        full[l] = b[k];
+        k ++;
+      }
+      else if (k == b.length) {
+        full[l] = a[j];
+        j ++;
+      }
+    }
+    return full;
   }//end merge()
 
 
@@ -33,7 +58,21 @@ public class MergeSort
    ******************************************************/
   public static int[] sort( int[] arr )
   {
-
+    if (arr.length <= 1) {
+      return arr;
+    }
+    int middle = arr.length / 2;
+    int[] left = new int[middle];
+    int[] right = new int[arr.length - middle];
+    for (int i = 0; i < middle; i ++) {
+      left[i] = arr[i];
+    }
+    for (int j = 0; j < right.length; j ++) {
+      right[j] = arr[middle + j];
+    }
+    left = sort(left);
+    right = sort(right);
+    return merge(left, right);
   }//end sort()
 
 
@@ -59,14 +98,13 @@ public class MergeSort
   //main method for testing
   public static void main( String [] args )
   {
-    /*~~~~~~~~~~~~~~ Ye Olde Tester Bar ~~~~~~~~~~~~~~
       int[] arr0 = {0};
       int[] arr1 = {1};
       int[] arr2 = {1,2};
       int[] arr3 = {3,4};
       int[] arr4 = {1,2,3,4};
       int[] arr5 = {4,3,2,1};
-      int[] arr6 = {9,42,17,63,0,512,23};
+      int[] arr6 = {0,9,17,23,42,63,512};
       int[] arr7 = {9,42,17,63,0,9,512,23,9};
 
       System.out.println("\nTesting mess-with-array method...");
@@ -85,6 +123,7 @@ public class MergeSort
       printArray( sort( arr5 ) );
       printArray( sort( arr6 ) );
       printArray( sort( arr7 ) );
+      /*~~~~~~~~~~~~~~ Ye Olde Tester Bar ~~~~~~~~~~~~~~
       ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
   }//end main()
 
