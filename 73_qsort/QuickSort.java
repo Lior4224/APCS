5137bf02fff823faf3bd3075cf27b06d48ed30b5
Lior Polischouk <67924209+Lior4224@users.noreply.github.com>
2022-03-09 21:16:05 -0500

    Update QuickSort.java

--- 73_qsort/QuickSort.java
+++ 73_qsort/QuickSort.java
@@ -1,22 +1,30 @@
-//Clyde Sinclair
-//APCS pd0
-//HW72 -- QuickSort
+//Team PJOLLN: Neil Lin, Lior Polischouk, Joseph Othman
+//APCS pd7
+//HW73 -- QuickSort
 //2022-03-09w
-//time spent: _h
+//time spent: 0.5 h
 
 /***
  * class QuickSort
  * Implements quicksort algo to sort an array of ints in place
  *
  * 1. Summary of QuickSort algorithm:
- * QSort(arr):
+ * QSort(arr): Take arr, and perform a partition, choosing the pivot by taking
+ the median of 3 randomly chosen entries of the array.
+ Then, QSort on the left and right partitions of the current array. Continue until
+ done.
+
+ Why we choose the median of 3 random entries of the array:
+ This is not deterministic, so it will not ALWAYS be bad for an array, and choosing
+ the median of the three means that we'll be less likely to be near the extremes).
  *
  * 2a. Worst pivot choice and associated run time:
- *
+ * Minimum or maximum element. O(n^2).
  * 2b. Best pivot choice and associated run time:
- *
+ * Median element. O(nlogn).
  * 3. Approach to handling duplicate values in array:
- *
+ *  Throw in a less than or equal to instead of a less than witihin the partition
+ method.
  **/
 
 public class QuickSort
@@ -69,19 +77,66 @@ public class QuickSort
    */
   public static void qsort( int[] d )
   {
-
+    qsort(d, 0, d.length - 1);
   }
 
   //you may need a helper method...
 
+  public static void qsort( int[] d, int loPos, int hiPos)
+  {
+    if(loPos > -1 && hiPos < d.length && loPos < hiPos)
+    {
+      int x = partition(d, loPos, hiPos);
+      qsort(d, loPos, x-1); //qsort on the left partition
+      qsort(d, x+1, hiPos); //qsort on the right partition
+    }
+  }
 
-  
+  public static int partition( int arr[], int loPos, int hiPos)
+  {
+    int pvtPos;
+    // Let's choose 3 elements and take their median. This will correspond to the pivot element.
+    // This helps in avoiding the worst case, most of the time; this is not deterministic, so
+    // On average this works pretty fast.
+
+    int x = loPos + (int) (Math.random()*(hiPos-loPos+1));
+    int y = loPos + (int) (Math.random()*(hiPos-loPos+1));
+    int z = loPos + (int) (Math.random()*(hiPos-loPos+1));
+
+    if((x < y && y < z) || (x > y && y > z)) {
+      pvtPos = y;
+    }
+
+    else if ((x < z && z < y) || (x > z && z > y)) {
+      pvtPos = z;
+    }
+
+    else {
+      pvtPos = x;
+    }
+
+    int v = arr[pvtPos];
+
+    swap( pvtPos, hiPos, arr);
+    int s = loPos;
+
+    for( int i = loPos; i < hiPos; i++ ) {
+      if ( arr[i] <= v) {
+        swap( i, s, arr );
+        s++;}
+    }
+    swap(s,hiPos,arr);
+
+    return s;
+  }//end partition
+
+
+
 
 
   //main method for testing
   public static void main( String[] args )
   {
-    /*~~~~s~l~i~d~e~~~m~e~~~d~o~w~n~~~~~~~~~~~~~~~~~~~~ (C-k, C-k, C-y) 
 
     //get-it-up-and-running, static test case:
     int [] arr1 = {7,1,5,12,3};
@@ -107,10 +162,9 @@ public class QuickSort
     qsort( arrN );
     System.out.println("arrN after sort: " );
     printArr(arrN);
+    /*~~~~s~l~i~d~e~~~m~e~~~d~o~w~n~~~~~~~~~~~~~~~~~~~~ (C-k, C-k, C-y)
     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 
-
-    /*~~~~s~l~i~d~e~~~m~e~~~d~o~w~n~~~~~~~~~~~~~~~~~~~~ (C-k, C-k, C-y)
 
     //get-it-up-and-running, static test case w/ dupes:
     int [] arr2 = {7,1,5,12,3,7};
@@ -137,6 +191,7 @@ public class QuickSort
     qsort( arrMatey );
     System.out.println("arrMatey after sort: " );
     printArr(arrMatey);
+    /*~~~~s~l~i~d~e~~~m~e~~~d~o~w~n~~~~~~~~~~~~~~~~~~~~ (C-k, C-k, C-y)
     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 
   }//end main
