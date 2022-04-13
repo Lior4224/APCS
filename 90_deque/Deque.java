bd9954c0e926b5dcd30a6a9430f87e93b81ab6f8
Lior Polischouk <67924209+Lior4224@users.noreply.github.com>
2022-04-13 12:46:35 -0400

    Update Deque.java

--- 90_deque/Deque.java
+++ 90_deque/Deque.java
@@ -1,3 +1,15 @@
+/***
+Team PJOLLN: Neil Lin, Lior Polischouk, Joseph Othman
+APCS pd7
+HW90 --
+2022-04-12
+time spent: 0.5 hrs
+DISCO:
+- 
+QCC:
+- Do we need methods on the interface such as offer and poll?
+
+**/
 public interface Deque<T> {
   public void addFirst(T x);
 
